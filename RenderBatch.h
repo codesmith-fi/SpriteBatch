d08e484c6947b3e9d084f8411fd8433661130b1e
Erno Pakarinen <codesmith.fi@gmail.com>
2021-06-03 00:17:15 +0300

    Initial RenderBatch draws decals without Z ordering

---
+++ RenderBatch.h
@@ -0,0 +1,156 @@
+/*
+	olcPGEX_RenderBatch.h
+	+-------------------------------------------------------------+
+	|         OneLoneCoder Pixel Game Engine Extension            |
+	|                RenderBatch - v0.1.0			              |
+	+-------------------------------------------------------------+
+
+	What is this?
+	~~~~~~~~~~~~~
+	This is an extension to Pixel Game Engine and aims to implement
+	a simple texture atlas (or sprite sheet). Texture atlas is basically
+	one big sprite having multiple sub images. These sub images can
+	be drawn separately to form textures or animated sprites.
+	This class does not animate anything by itself, you must ask the
+	Draw() call to draw sub frames separately.
+
+	See the source file TextureAtlasAppPGE.cpp for an example how to use.
+
+	License (OLC-3)
+	~~~~~~~~~~~~~~~
+
+	Copyright 2018 - 2021 OneLoneCoder.com
+
+	Redistribution and use in source and binary forms, with or without modification,
+	are permitted provided that the following conditions are met:
+
+	1. Redistributions or derivations of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+
+	2. Redistributions or derivative works in binary form must reproduce the above
+	copyright notice. This list of conditions and the following	disclaimer must be
+	reproduced in the documentation and/or other materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the names of its contributors may
+	be used to endorse or promote products derived from this software without specific
+	prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS	"AS IS" AND ANY
+	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+	OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+	SHALL THE COPYRIGHT	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+	INCIDENTAL,	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+	BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+	SUCH DAMAGE.
+
+	Links
+	~~~~~
+	Homepage:	https://github.com/codesmith-fi
+	Mail:		codesmith.fi@gmail.com
+
+	Author
+	~~~~~~
+	Erno Pakarinen (aka xanaki#9472 in discord, aka codesmith-fi)
+
+*/
+
+#ifndef __RENDERBATCH_H_DEFINED__
+#define __RENDERBATCH_H_DEFINED__
+
+#include <list>
+#include <memory>
+
+namespace olc {
+
+class Camera
+{
+public: // Methods
+	Camera() { };
+	Camera(const olc::vf2d& pos, const olc::vf2d& vs) :
+		position(pos), viewsize(vs) { };
+	void moveTo(const olc::vf2d& newpos) {
+		position = newpos;
+	}
+public: // Data
+	olc::vf2d position;
+	olc::vf2d viewsize;
+};
+
+class RenderBatchEntry
+{
+public:
+	RenderBatchEntry(
+		std::unique_ptr<olc::Decal> d, 
+		const olc::vf2d& p,
+		const olc::vf2d& s,
+		float order) : decal(std::move(d)), position(p), depth(order), scale(s) {
+	}
+	virtual ~RenderBatchEntry() {};
+
+	std::unique_ptr<olc::Decal> decal;
+	float depth;
+	olc::vf2d scale;
+	olc::vf2d position;
+};
+
+class RenderBatch : public olc::PGEX
+{
+public: // Enums
+	enum class DrawOrder {
+		ASCENDING,
+		DESCENDING,
+		UNORDERED
+	};
+
+public:
+	RenderBatch(const DrawOrder& order = DrawOrder::ASCENDING) :
+		m_order(order) {};
+	virtual ~RenderBatch() {}
+
+	void SetOrder(const DrawOrder& order) {
+		m_order = order;
+	}
+
+	void Begin() {
+		m_drawables.clear();
+	}
+
+	void End() {
+		for (auto i = m_drawables.begin(); i != m_drawables.end(); ++i) {
+			olc::vf2d p = i->get()->position;
+			olc::vf2d s = i->get()->scale;
+			olc::Decal* d= i->get()->decal.get();
+			pge->DrawDecal(p, d, s);
+		}
+	}
+
+	void Draw(std::shared_ptr<olc::Sprite> sprite,
+		const olc::vf2d& pos, const olc::vf2d& scale, std::float_t depth) {
+
+		std::unique_ptr<olc::Decal> decal = std::make_unique<olc::Decal>(sprite.get());
+		std::unique_ptr<RenderBatchEntry> sbe = std::make_unique<RenderBatchEntry>(
+			std::move(decal),
+			pos,
+			scale,
+			depth);
+		m_drawables.push_back(std::move(sbe));
+
+		// TODO: Ordering
+		/*
+		for (auto i = m_drawables.begin(); i != m_drawables.end(); ++i) {
+
+		}
+		*/
+	}
+
+private:
+	std::list<std::unique_ptr<RenderBatchEntry>> m_drawables;
+	DrawOrder m_order;
+};
+
+} // namespace olc
+
+#endif // __RENDERBATCH_H_DEFINED__
---
+++ RenderBatchDemo.cpp
@@ -0,0 +1,78 @@
+/** 
+ * SpriteBatch test application
+ * Author: Erno Pakarinen (codesmith.fi@gmail.com)
+ */
+
+#include <iostream>
+#include <memory>
+
+#define OLC_PGE_APPLICATION
+#include "pge/olcPixelGameEngine.h"
+#include "RenderBatch.h"
+#include "DebugLogger.h"
+
+ // Override base class with your custom functionality
+class PGEApplication : public olc::PixelGameEngine
+{
+public:
+	PGEApplication()
+	{
+		LOG_INFO() << "PGEApplication created";
+		sAppName = "PGEApplication - SpriteBatch demo";
+	}
+
+	virtual ~PGEApplication() {
+		LOG_INFO() << "PGEApplication destroyed";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		LOG_INFO() << "PGEApplication::OnUserCreate() initializing";
+		m_Sprite1 = std::make_shared<olc::Sprite>("Assets\\desert.png");
+		m_Sprite2 = std::make_shared<olc::Sprite>("Assets\\soniccd.png");
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		m_RenderBatch.Begin();
+		m_RenderBatch.Draw(
+			m_Sprite1,
+			olc::vf2d(0.0f, 0.0f),
+			olc::vf2d(1.0f, 1.0f),
+			1.0f
+		);
+		m_RenderBatch.Draw(
+			m_Sprite2,
+			olc::vf2d(50.0f, 50.0f),
+			olc::vf2d(1.0f, 1.0f),
+			1.0f
+			);
+		m_RenderBatch.End();
+		return true;
+	}
+
+	/**
+	 * If OnUserDestroy() returns false, the application will not exit,
+	 * it keeps running.
+	 */
+	bool OnUserDestroy() override
+	{
+		LOG_INFO() << "PGEApplication::OnUserDestroy()";
+		return true;
+	}
+
+private:
+	olc::RenderBatch m_RenderBatch;
+	std::shared_ptr<olc::Sprite> m_Sprite1;
+	std::shared_ptr<olc::Sprite> m_Sprite2;
+};
+
+int main()
+{
+	PGEApplication demo;
+	if (demo.Construct(256, 240, 4, 4))
+		demo.Start();
+	return 0;
+}
